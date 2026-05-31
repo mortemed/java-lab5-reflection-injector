a77ebf562853ef9ea32d77d7b82c260ba28b8bdf
VERTER <daniil.polichnoy@gmail.com>
2026-05-31 21:44:18 +0300

    starting this for the last time, but not least

---
+++ src/main/java/ru/vsu/polichnoy/lab5/Main.java
@@ -0,0 +1,12 @@
+package ru.vsu.polichnoy.lab5;
+
+/**
+ * Консольная программа для демонстрации внедрения зависимостей через reflection.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Лабораторная работа №5");
+        System.out.println("Внедрение зависимостей с помощью reflection");
+    }
+}
